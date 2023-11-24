09be20dcc35bc3a96c463edaa9baedd547036727
Adam Gerber <adam.gerber@yahoo.com>
2023-11-24 08:53:40 -0600

    clean up sounds

--- pythonic/mvc/controller/Sound.py
+++ pythonic/mvc/controller/Sound.py
@@ -37,17 +37,8 @@ class Sound:
         #pass the above lambda to thread-pool, along with the path to file
         Sound.soundExecutor.submit(run, strPath)
 
-    # todo: thrust and background music not looping properly.
-    @staticmethod
-    def clipForLoopFactory(fileName):
-        try:
-            wave_obj = sa.WaveObject.from_wave_file(fileName)
-            return wave_obj
-        except Exception as e:
-            pass
 
 
-
-if __name__ == "__main__":
-    cwd = "/".join(os.getcwd().split('/')[:-2])
-    Sound.clipForLoopFactory(cwd+"/resources/sounds/whitenoise.wav")
+# if __name__ == "__main__":
+#     cwd = "/".join(os.getcwd().split('/')[:-2])
+#     Sound.clipForLoopFactory(cwd+"/resources/sounds/whitenoise.wav")
