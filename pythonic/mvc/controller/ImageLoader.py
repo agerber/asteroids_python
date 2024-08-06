596f8c9b5b0d7ff4608ca6f4496da14c377c909b
Abhiyankar Shakti <abhi.shakti123@gmail.com>
2024-08-06 16:19:59 +0530

    do not load seris

--- pythonic/mvc/controller/ImageLoader.py
+++ pythonic/mvc/controller/ImageLoader.py
@@ -44,8 +44,8 @@ class ImageLoader:
             files = os.listdir(currentPath)
             for file in files:
                 split_text = os.path.splitext(file)
-                if split_text[1] == '.png':
-                    # print(os.path.join(currentPath,file))
+                if split_text[1] == '.png' and 'do_not_load' not in split_text[0]:
+                    print(os.path.join(currentPath,file))
                     pngImages[split_text[0]] = Sprite.loadGraphic(os.path.join(currentPath,file))
 
         return pngImages
